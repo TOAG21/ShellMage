af33ea688d805f9b38d0527b849b5411427ef4ab
Sam <samuel.heasley@mymail.champlain.edu>
2025-02-04 20:02:16 -0500

    real work

--- GlacierRunner/GlacierRunner/Assets/Scripts/Driving.cs
+++ GlacierRunner/GlacierRunner/Assets/Scripts/Driving.cs
@@ -6,6 +6,17 @@ public class Driving : MonoBehaviour
 {
     Rigidbody2D rb;
 
+    float acceleration = 1f;
+    float torque = 0.1f;
+
+    float leftTrackStatus = 0.0f;
+    float rightTrackStatus = 0.0f;
+    float trackAccelerationTime = 1.75f;
+
+    float reverseCoefficient = -0.7f;
+
+    float speedLimit = 50f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +26,66 @@ public class Driving : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.W))
+        float diminishingSpeed = 1 - ( rb.velocity.magnitude / speedLimit);
+
+        if(Input.GetMouseButton(0))//left forward
         {
-            rb.AddForce(new Vector2(1f, 0f));
+            leftTrackStatus += trackAccelerationTime * Time.deltaTime;
+            if (leftTrackStatus > 1.0f) { leftTrackStatus = 1.0f; }
+
+            //if (leftTrackStatus >= 0.0f)
+            {
+                rb.AddForce(this.transform.up * acceleration * leftTrackStatus * diminishingSpeed);
+                rb.AddTorque(-1f * torque * leftTrackStatus);
+            }
         }
+        else if (Input.GetKey(KeyCode.Q))//left reverse
+        {
+            leftTrackStatus -= trackAccelerationTime * Time.deltaTime;
+            if (leftTrackStatus < -1.0f) { leftTrackStatus = -1.0f; }
+
+            //if (leftTrackStatus <= 0.0f)
+            {
+                rb.AddForce(-1f * reverseCoefficient * this.transform.up * acceleration * leftTrackStatus * diminishingSpeed);
+                rb.AddTorque(reverseCoefficient * torque * leftTrackStatus);
+            }
+        }
+        else
+        {
+            if (leftTrackStatus > 0.1f) { leftTrackStatus -= trackAccelerationTime * Time.deltaTime; }
+            if (leftTrackStatus < 0.1f) { leftTrackStatus += trackAccelerationTime * Time.deltaTime; }
+
+        }
+
+        if (Input.GetMouseButton(1))//right forward
+        {
+            rightTrackStatus += trackAccelerationTime * Time.deltaTime;
+            if (rightTrackStatus > 1.0f) { rightTrackStatus = 1.0f; }
+
+           // if (rightTrackStatus >= 0.0f)
+            {
+                rb.AddForce(this.transform.up * acceleration * rightTrackStatus * diminishingSpeed);
+                rb.AddTorque(torque * rightTrackStatus);
+            }
+        }
+        else if (Input.GetKey(KeyCode.E))//right reverse
+        {
+            rightTrackStatus -= trackAccelerationTime * Time.deltaTime;
+            if (rightTrackStatus < -1.0f) { rightTrackStatus = -1.0f; }
+
+            //if (rightTrackStatus <= 0.0f)
+            {
+                rb.AddForce(-1f * reverseCoefficient * this.transform.up * acceleration * rightTrackStatus * diminishingSpeed);
+                rb.AddTorque(-1f * reverseCoefficient * torque * rightTrackStatus);
+            }
+        }
+        else
+        {
+            if (rightTrackStatus > 0.1f) { rightTrackStatus -= trackAccelerationTime * Time.deltaTime; }
+            if (rightTrackStatus < 0.1f) { rightTrackStatus += trackAccelerationTime * Time.deltaTime; }
+
+        }
+
+        //Debug.Log(this.transform.up);
     }
 }
